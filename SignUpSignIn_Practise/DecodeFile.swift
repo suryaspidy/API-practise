1c88570f0cba92033438f02eac7229881b930d35
suryaspidy <ss0972203@gmail.com>
2021-05-23 11:30:25 -0700

    Third commit

---
+++ SignUpSignIn_Practise/DecodeFile.swift
@@ -0,0 +1,14 @@
+//
+//  DecodeFile.swift
+//  SignUpSignIn_Practise
+//
+//  Created by surya-zstk231 on 23/05/21.
+//
+
+import Foundation
+
+struct DecodeFile: Codable{
+    let news_id: String
+    let name: String
+    let url: String
+}
--- SignUpSignIn_Practise/HomeVc.swift
+++ SignUpSignIn_Practise/HomeVc.swift
@@ -20,40 +20,35 @@ class HomeVc: UIViewController {
         apiCall()
     }
     
-
-    func apiCalling(){
-        let headers = [
-            "x-rapidapi-key": "921527fb95msh93e9942a87b43a0p150366jsn7fba59d8b6fa",
-            "x-rapidapi-host": "google-news1.p.rapidapi.com"
-        ]
-
-        let request = NSMutableURLRequest(url: NSURL(string: "https://google-news1.p.rapidapi.com/topic-headlines?topic=WORLD&country=US&lang=en")! as URL,
-                                                cachePolicy: .useProtocolCachePolicy,
-                                            timeoutInterval: 10.0)
-        request.httpMethod = "GET"
-        request.allHTTPHeaderFields = headers
-
-        let session = URLSession.shared
-        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-            if (error != nil) {
-                print(error)
-            } else {
-                let httpResponse = response as? HTTPURLResponse
-                print(httpResponse)
-            }
-        })
-
-        dataTask.resume()
-    }
     
     func apiCall(){
-//        let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page")!
         let url = URL(string: "https://hubblesite.org/api/v3/news?page=all")!
         let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: url) { (data, response, error) in
-            print(response)
+        let task = session.dataTask(with: url) { [self] (data, response, error) in
+            if error == nil{
+                if let resultData = data {
+                    decodeDataFunc(data: resultData)
+                }
+            }
         }
         task.resume()
+    }
+    
+    func decodeDataFunc(data: Data){
+        let decode = JSONDecoder()
+        do{
+            let stringifyData = try decode.decode(DecodeFile.self, from: data)
+            
+            //let name = stringifyData.name
+            //let id = stringifyData.news_id
+            //let url = stringifyData.url
+            
+            //print("\(name) ")
+            //print(stringifyData)
+        }catch{
+            print(error.localizedDescription)
+        }
+        
     }
 
     @IBAction func profileBtnPressed(_ sender: UIBarButtonItem) {
